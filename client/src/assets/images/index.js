e152f411c59ea00809a7ac023e79b32e9c3b8347
mkallgren08 <mkallgren08@gmail.com>
2018-12-21 17:07:41 -0500

    Added in collapse functionality to menu on the side

---
+++ client/src/assets/images/index.js
@@ -0,0 +1,40 @@
+// Backgrounds
+import acidBckgrnd from "./resmap-acid.png";
+import alumBckgrnd from "./resmap-Al.png";
+import blackTiBckgrnd from "./resmap-blackTi.png";
+import coalBckgrnd from "./resmap-coal.png";
+import oilBckgrnd from "./resmap-oil.png";
+
+
+// Icons
+import acidIcon from "./icon-acid.png"
+import alumIcon from "./icon-Al.png"
+import blackTiIcon from "./icon-blackTi.png"
+import coalIcon from "./icon-coal.png"
+import oilIcon from "./icon-oil.png"
+
+const images = {
+  backgrounds:{
+    acid: acidBckgrnd,
+    alum: alumBckgrnd,
+    blackTi: blackTiBckgrnd,
+    coal: coalBckgrnd,
+    oil: oilBckgrnd,
+  },
+  icons:{
+    acid: acidIcon,
+    alum: alumIcon,
+    blackTi: blackTiIcon,
+    coal: coalIcon,
+    oil: oilIcon,
+  },
+  colors:{
+    acid: "#6ABD49",
+    alum: "#5CB3FF",
+    blackTi: "#524B4B",
+    coal: "#CC3F02",
+    oil: "#0000FF",
+  }
+}
+
+export default images;
